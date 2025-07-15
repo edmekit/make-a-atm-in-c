36a29c4ab43458c1b3559b797946fef4da67adb5
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-15 17:45:27 +0800

    added user input validation

--- ui.cpp
+++ ui.cpp
@@ -2,86 +2,81 @@
 
 using namespace std;
 
-int pass, i,choice, action, ex;
+int pass, i,choice, action, ex, with, depo;
+bool running = true;
+
+int IntOnly(const string& s)
+{   
+    int act;
+    while (true)
+    {
+        cout << s << endl;
+        cin >> act;
+        
+        if (cin.fail())
+        {
+            cout << "Invalid input. Number only" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+            continue;
+        }
+        return act;
+    }
+}
 
 int main()
 {
-    cout << "Enter your PIN NO: " << endl;
-    cin >> pass;
-
-    if (pass == 1234)
+    i = 0;
+    pass = 1234;
+    while (true)
     {
-        cout << "Choose from the following options" << endl;
-        cout << "1. Withdrawal" << endl;
-        cout << "2. Deposit" << endl;
-        cout << "3. Balance" << endl;
-        cout << "4. Exit" << endl;
-        cin >> choice;
-
-        if (choice == 1)
+        int pin = IntOnly("Enter your PIN");
+        
+        if (pin == pass)
         {
-            cout << "Enter the amount you want to withdraw" << endl;
-            cin >> i;
-            cout << "You have withdrawn " << i << endl;
-            cout << "1. End transaction" << endl;
-            cout << "2. Go back" << endl;
-            cin >> action;
-            if (action == 1)
-                exit(0);
-            else if (action == 2)
+            break;
+        }
+        else
+        {
+            cout << "Incorrect PIN. Try again" << endl;
+        }
+    }  
+        while (running == true)
+        {
+            cout << "Choose from the following options" << endl;
+            cout << "1. Withdrawal" << endl;
+            cout << "2. Deposit" << endl;
+            cout << "3. Balance" << endl;
+            cout << "4. Exit" << endl;
+            cin >> choice;
+
+            if (choice == 1)
             {
-                cout << "Choose from the following options" << endl;
-                cout << "1. Withdrawal" << endl;
-                cout << "2. Deposit" << endl;
-                cout << "3. Balance" << endl;
-                cout << "4. Exit" << endl;
-                cin >> choice;
+                cout << "Enter the amount you want to withdraw" << endl;
+                cin >> with;
+                i -= with;
+                cout << "You have withdrawn " << with << endl;    
+            }
+
+            else if (choice == 2)
+            {
+                cout << "Enter the amount you want to deposit" << endl;
+                cin >> depo;
+                i += depo;
+                cout << "You have deposited " << depo << endl;
+            }
+
+            else if (choice == 3)
+            {
+                cout << "Your balance is " << i << endl;
+            }
+
+            else if (choice == 4)
+            {
+                running = false;
             }
         }
+        
 
-        else if (choice == 2)
-        {
-            cout << "Enter the amount you want to deposit" << endl;
-            cin >> i;
-            cout << "You have deposited " << i << endl;
-            cout << "1. End transaction" << endl;
-            cout << "2. Go back" << endl;
-            cin >> action;
-            if (action == 1)
-                exit(0);
-            else if (action == 2)
-            {
-                cout << "Choose from the following options" << endl;
-                cout << "1. Withdrawal" << endl;
-                cout << "2. Deposit" << endl;
-                cout << "3. Balance" << endl;
-                cout << "4. Exit" << endl;
-                cin >> choice;
-            }
-        }
-
-        else if (choice == 3)
-        {
-            cout << "Your balance is " << i << endl;
-            cout << "1. End transaction" << endl;
-            cout << "2. Go back" << endl;
-            cin >> action;
-            if (action == 1)
-                exit(0);
-            else if (action == 2)
-            {
-                cout << "Choose from the following options" << endl;
-                cout << "1. Withdrawal" << endl;
-                cout << "2. Deposit" << endl;
-                cout << "3. Balance" << endl;
-                cout << "4. Exit" << endl;
-                cin >> choice;
-            }
-        }
-
-        else if (choice == 4)
-        {
-            exit(0);
-        }
-    }
+        
 }
