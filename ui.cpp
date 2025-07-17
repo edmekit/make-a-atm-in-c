7478fd0b6b3299e27b961bfb75f3e8f4bf1ae89a
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-17 19:48:40 +0800

    improve ux

--- ui.cpp
+++ ui.cpp
@@ -47,11 +47,13 @@ int main()
     users[4].balance = 40;
  
     while (true)
-    {
+    {   
+        cout << endl;
+        cout << "Welcome to our bank" << endl;
+        cout << endl;
         int pin = IntOnly("Enter your PIN");
         userIndex = -1;
-
-        
+  
         for (int i = 0; i < 5; i++)
         {
             if (pin == users[i].pin)
@@ -68,49 +70,64 @@ int main()
         }
 
         bal = users[userIndex].balance;
+        running = true;
 
-            while (running == true)
-            {
+            while (running)
+            {   
+                cout << endl;
                 cout << "Choose from the following options" << endl;
                 cout << "1. Withdrawal" << endl;
                 cout << "2. Deposit" << endl;
                 cout << "3. Balance" << endl;
                 cout << "4. Exit" << endl;
+                cout << endl;
                 cin >> choice;
 
                 if (choice == 1)
-                {
+                {   
+                    cout << endl;
                     with = IntOnly("Enter the amount you want to withdraw:");
                     if (with > bal)
-                    {
+                    {   
+                        cout << endl;
                         cout << "Insufficient balance" << endl;
+                        cout << endl;
                         continue;
                     }
                     else
                     {
                         bal -= with;
+                        cout << endl;
                         cout << "You have withdrawn " << with << endl;
+                        cout << endl;
                     }
                         
                 }
 
                 else if (choice == 2)
-                {
+                {   
+                    cout << endl;
                     depo = IntOnly("Enter the amount you want to deposit:");
                     bal += depo;
+                    cout << endl;
                     cout << "You have deposited " << depo << endl;
+                    cout << endl;
                 }
 
                 else if (choice == 3)
-                {
+                {   
+                    cout << endl;
                     cout << "Your balance is " << bal << endl;
+                    cout << endl;
                 }
 
                 else if (choice == 4)
-                {
+                {   
+                    cout << endl;
                     cout << "Are you sure?" << endl;
                     cout << "1. Yes" << endl;
                     cout << "2. No" << endl;
+                    cout << endl;
                     cin >> ex;
                     if (ex == 1)
                     {
@@ -122,6 +139,8 @@ int main()
                     }
                 }
             }
+        users[userIndex].balance = bal;
+        cout << endl;
         cout << "Thank you for banking with us" << endl; 
         break;
         }
